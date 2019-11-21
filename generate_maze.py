ebc4e5843e8c5afdf2b5a01a94b735d7d5abee51
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-11-20 21:39:16 -0800

    creating png

---
+++ generate_maze.py
@@ -0,0 +1,10 @@
+from pnghandler import PngHandler
+
+
+def main():
+    handler = PngHandler((200, 200))
+    handler.setup_grid()
+
+
+if __name__ == "__main__":
+    main()
--- pnghandler.py
+++ pnghandler.py
@@ -2,11 +2,12 @@ from PIL import Image
 from typing import Tuple
 
 
-class PngHandler:
+class PngHandler(object):
 
-    def __init__(self, png):
-        self.pixel_matrix = self.import_png(png)
-    
+    def __init__(self, size):
+        self.size = size
+        self.pixel_matrix = Image.new('RGB', size)
+
     @staticmethod
     def import_png(png: str) -> Image.Image:
         return Image.open(png).convert('RGB')
@@ -15,3 +16,10 @@ class PngHandler:
     def get_rgb(x: int, y: int, image: Image.Image) -> Tuple:
         return tuple(image.getpixel((x, y)))
 
+    def setup_grid(self):
+        for x in range(self.size[0]):  # loop through row
+            for y in range(self.size[1]):  # loop through column
+                print("x: ", x, "y: ", y, self.get_rgb(x, y, self.pixel_matrix))
+
+    def show(self):
+        self.pixel_matrix.show()
