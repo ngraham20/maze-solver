0e27dbc2f5038001f1bed44ca3e830db38f1df75
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-11-21 17:02:00 -0800

    nodes can connect

--- generate_maze.py
+++ generate_maze.py
@@ -1,10 +1,20 @@
-from pnghandler import PngHandler
+from maze import Maze
 
 
 def main():
-    handler = PngHandler(21)
-    handler.setup_grid()
-    handler.image.save("test.png", "PNG")
+    maze = maze(21)
+    maze.setup_grid()
+
+    matrix = maze.matrix
+
+    # maze.connect(matrix[1][1], matrix[1][3])
+    # maze.connect(matrix[3][5], matrix[3][7])
+    # maze.connect(matrix[5][5], matrix[7][5])
+    # maze.connect(matrix[1][9], matrix[3][9])
+
+
+
+    maze.image.save("pngs/test.png", "PNG")
 
 
 if __name__ == "__main__":
