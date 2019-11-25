23f9a79a5f4f8560806674075ccd0892085d54e8
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-11-25 01:42:42 -0800

    ipmlemented a-star search

--- solve_maze.py
+++ solve_maze.py
@@ -1,14 +1,22 @@
 from solverb import SolveRB
+from solveastar import SolveAStar
 
 
 def main():
-     rb = SolveRB()
-     rb.import_png("pngs/101x101-RB-0.01905s.png")
-     the_stack = rb.solve((1, 1), (99, 99))
+     # rb = SolveRB()
+     # rb.import_png("pngs/501x501-RB-0.22689s.png")
+     # the_stack = rb.solve((1, 1), (499, 499))
+     #
+     # #print(str(the_stack))
+     #
+     # rb.save_solution(the_stack)
 
-     print(str(the_stack))
+     star = SolveAStar()
+     star.import_png("pngs/1001x1001-RB-1.12744s.png")
+     solution = star.solve((1, 1), (999, 999))
 
-     rb.save_solution(the_stack)
+     solution.reverse()
+     star.save_solution(solution)
 
 
 if __name__ == "__main__":
---
+++ solveastar.py
@@ -0,0 +1,99 @@
+# A* search algorithm for solving the maze
+from functools import total_ordering
+from mazesolver import MazeSolver
+import math
+import time
+
+
+class SolveAStar(MazeSolver):
+
+    def __init__(self):
+        super().__init__()
+        self.name = "solutionAStar"
+
+    def solve(self, beginning, end):
+        start_time = time.time()
+
+        the_open = {}
+        the_closed = set()
+
+        # start at beginning
+        # add beginning to open set
+        the_open[beginning] = Node(beginning)
+
+        while the_open:
+            node = min(the_open.values())
+            the_open.pop(node.location, None)
+            the_closed.add(node.location)
+            x, y = node.location
+
+            if (x, y) == end:
+                self.duration = time.time() - start_time
+                return node.ancestry
+
+            successors = []
+
+            # generate children nodes and add to open set (N,S,E,W)
+            # check north
+            if y - 1 > 0 and self.maze.image.getpixel((x, y-1)) == (255, 255, 255):
+                successors.append(Node((x, y - 1), node))
+
+            # check south
+            if y + 1 < self.maze.size and self.maze.image.getpixel((x, y+1)) == (255, 255, 255):
+                successors.append(Node((x, y + 1), node))
+
+            # check east
+            if x - 1 > 0 and self.maze.image.getpixel((x - 1, y)) == (255, 255, 255):
+                successors.append(Node((x - 1, y), node))
+
+            # check west
+            if x + 1 < self.maze.size and self.maze.image.getpixel((x + 1, y)) == (255, 255, 255):
+                successors.append(Node((x + 1, y), node))
+
+            for child in successors:
+                if child.location not in the_closed:
+                    if child.parent:
+                        child.g = node.g + 1  # distance to start through ancestry. 0 if beginning
+                    else:
+                        child.g = 0
+
+                    child.h = abs(child.location[0] - end[0]) + abs(child.location[1] - end[1])  # manhattan distance to finish
+                    child.f = int((child.g + child.h))  # (g + h)
+                    if child not in the_open:
+                        the_open[child.location] = child
+                    else:
+                        #open_neighbor = [neighbor for neighbor in the_open if neighbor.location == child.location][0]
+                        open_neighbor = the_open[child.location]
+                        if child.g < open_neighbor.g:
+                            open_neighbor.g = child.g
+                            open_neighbor.parent = child.parent
+
+            the_closed.add(node.location)
+
+
+class Node:
+
+    def __init__(self, location, parent=None):
+        self.parent = parent
+        self.location = location
+
+        self.ancestry = [self.location]
+
+        if self.parent is not None:
+            self.ancestry += self.parent.ancestry
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.location == other.location
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def __gt__(self, other):
+        return self.f > other.f
+
+    def __hash__(self):
+        return hash(self.location)
