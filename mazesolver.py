ce69ae998f9fc764b2d42548a2cfd090ca34b03a
Nathaniel Graham <ngraham20@my.whitworth.edu>
2019-11-28 14:36:30 -0800

    added gif to solution

--- mazesolver.py
+++ mazesolver.py
@@ -1,5 +1,5 @@
 from maze import Maze
-
+import os
 
 class MazeSolver:
 
@@ -20,9 +20,22 @@ class MazeSolver:
     def solve(self, beginning, end):
         pass
 
-    def save_png(self, image):
+    def generate_png(self, image):
         image.save("./solutions/" + self.name + "-" + str(self.maze.size) + "x" + str(self.maze.size) + "-" +
                    str(float("%.5f" % self.duration)) + "s.png", "PNG")
+
+    def save_gif(self, image, frame_number):
+        dir_name = "solutions/" + self.name + "-" + str(self.maze.size) + "x" + str(self.maze.size) + "-" + \
+                   str(float("%.5f" % self.duration)) + "s"
+
+        try:
+            # Create target Directory
+            os.mkdir(dir_name)
+            print("Directory ", dir_name,  " Created ")
+        except FileExistsError:
+            print("Directory ", dir_name,  " already exists")
+
+        image.save(dir_name + "/" + str(frame_number) + ".png", "PNG")
 
     def save_solution(self, solution):
         solution.reverse()
@@ -31,8 +44,10 @@ class MazeSolver:
         increment = 255 / section
         for index in range(section):  # from red to purple
             self.maze.draw.point(solution[index], (255, 0, int(0 + increment * index)))
+            self.save_gif(self.maze.image, index)
 
         for index in range(section + 1):  # from purple to blue
             self.maze.draw.point(solution[section + index], (int(255 - increment * index), 0, 255))
+            self.save_gif(self.maze.image, section + index)
 
-        self.save_png(self.maze.image)
+        self.generate_png(self.maze.image)
