ba9b37188568c1bf833d15bbe81f8aafcf6dfea8
Kelly OLivier <kellyolivier@Kellys-MBP.whitworth.edu>
2019-12-11 13:31:52 -0800

    Primms working

--- generateprimms.py
+++ generateprimms.py
@@ -4,8 +4,7 @@ import random
 import pngfunctions
 from typing import Tuple
 
-
-#questions
+# questions
 """
 start at 1,1 in array 
 tree starts as n/2 by n/2 nodes
@@ -28,35 +27,50 @@ class GeneratePrimms(MazeGenerator):
     def __init__(self, size):
         super().__init__(size)
         self.name = "Primms"
-        self.directory = "mazes/" + self.name + "-" + str(self.size) + "x" + str(self.size) + "-" + hex(random.randint(0, 500))
+        self.directory = "mazes/" + self.name + "-" + str(self.size) + "x" + str(self.size) + "-" + hex(
+            random.randint(0, 500))
 
     def generate(self, history_log=None):
 
         start_time = time.time()
 
         the_frontier = {(1, 1)}  # set
-        self.matrix[1][1].visited = True
 
         while the_frontier:
-            x, y = the_frontier.pop()
+
+            x, y = random.sample(the_frontier, 1)[0]  # pick next node and connect ot visited neighbor
+            the_frontier.remove((x, y))
+            self.matrix[x][y].visited = True
+
+            pool = []
+
             # check all directions for new frontiers, add to frontier
-            if y - 2 > 0 and not self.matrix[x][y - 2].visited:  # check north
-                the_frontier.append((x, y - 2))
+            if y - 2 > 0:
+                if not self.matrix[x][y - 2].visited:  # check north
+                    the_frontier.add((x, y - 2))
+                else:
+                    pool.append((x, y - 2))
+            if y + 2 < len(self.matrix):
+                if not self.matrix[x][y + 2].visited:  # check south
+                    the_frontier.add((x, y + 2))
+                else:
+                    pool.append((x, y + 2))
 
-            if y + 2 < len(self.matrix) and not self.matrix[x][y + 2].visited:  # check south
-                the_frontier.append((x, y + 2))
+            if x - 2 > 0:
+                if not self.matrix[x - 2][y].visited:  # check east
+                    the_frontier.add((x - 2, y))
+                else:
+                    pool.append((x - 2, y))
 
-            if x - 2 > 0 and not self.matrix[x - 2][y].visited:  # check east
-                the_frontier.append((x - 2, y))
-
-            if x + 2 < len(self.matrix) and not self.matrix[x + 2][y].visited:  # check west
-                the_frontier.append((x + 2, y))
-
-            if len(the_frontier) > 0:
-                next_node = random.choice(the_frontier)  # pick next node
-                self.maze.connect(self.matrix[x][y], self.matrix[next_node[0]][next_node[1]], history_log)  # Nath what do for history here?
-                self.matrix[next_node[0]][next_node[1]].visited = True
-                the_frontier.append(next_node)
+            if x + 2 < len(self.matrix):
+                if not self.matrix[x + 2][y].visited:  # check west
+                    the_frontier.add((x + 2, y))
+                else:
+                    pool.append((x + 2, y))
+            
+            if len(pool) > 0:
+                next_node = random.choice(pool)
+                self.maze.connect(self.matrix[x][y], self.matrix[next_node[0]][next_node[1]], history_log)
 
         self.duration = time.time() - start_time
 
@@ -71,9 +85,8 @@ class GeneratePrimms(MazeGenerator):
         pass
 
 
-
-generator = GeneratePrimms(11)
+generator = GeneratePrimms(500)
 generator.generate()
 generator.save_png()
 
-#to run right click file and run file
+# to run right click file and run file
