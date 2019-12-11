ef7bf576af7cb548fa158973b3774ac9782c3265
Kelly OLivier <kellyolivier@Kellys-MBP.whitworth.edu>
2019-12-11 12:00:54 -0800

    Preliminary code for primms

--- generateprimms.py
+++ generateprimms.py
@@ -25,59 +25,50 @@ heap for smallest/largest
 
 
 class GeneratePrimms(MazeGenerator):
-
     def __init__(self, size):
+        self.name = "Primms"
         super().__init__(size)
-        self.name = "RB"
-        self.directory = "mazes/" + self.name + "-" + str(self.size) + "x" + str(self.size) + "-" + hex(random.randint(0, 500))
 
-    def maze_generator_factory(self, size: int):
-        return GeneratePrimms(size)
+    def generate(self):
+        super().generate()
 
-    def generate(self, history_log=None):
-        start_time = time.time()
-
-        the_stack = [(1, 1)]
+        the_frontier = [(1, 1)] #queue
         self.matrix[1][1].visited = True
 
-        while the_stack:
-            x, y = the_stack.pop()
-            if history_log is not None:
-                history_log.append(("pop", (x, y)))
-            pool = []
+        while the_frontier
+            x, y = the_frontier.pop()
+            # check all directions for new frontiers, add to frontier
             if y - 2 > 0 and not self.matrix[x][y - 2].visited:  # check north
-                pool.append((x, y - 2))
+                the_frontier.push((x, y - 2))
 
             if y + 2 < len(self.matrix) and not self.matrix[x][y + 2].visited:  # check south
-                pool.append((x, y + 2))
+                the_frontier.push((x, y + 2))
 
             if x - 2 > 0 and not self.matrix[x - 2][y].visited:  # check east
-                pool.append((x - 2, y))
+                the_frontier.push((x - 2, y))
 
             if x + 2 < len(self.matrix) and not self.matrix[x + 2][y].visited:  # check west
-                pool.append((x + 2, y))
+                the_frontier.push((x + 2, y))
 
-            if len(pool) > 0:
-                the_stack.append((x, y))
-                # if history_log is not None:
-                #     history_log.append(("push", (x, y)))
-                next_node = random.choice(pool)
-                self.maze.connect(self.matrix[x][y], self.matrix[next_node[0]][next_node[1]], history_log)
+            if len(the_frontier) > 0:
+                next_node = the_frontier.pop() # pick next node
+                self.maze.connect(self.matrix[x][y], self.matrix[next_node[0]][next_node[1]], None)  # Nath what do for history here?
                 self.matrix[next_node[0]][next_node[1]].visited = True
-                the_stack.append(next_node)
-                # if history_log is not None:
-                #     history_log.append(("push", (x, y)))
+                the_frontier.push(next_node)
 
-        self.duration = time.time() - start_time
-        if history_log is not None:
-            history_log.append(("pop", (1, 1)))
-            self.save_history(history_log)
 
     def save_png(self):
-        self.maze.image.save("mazes/RB-" + str(self.maze.size) + "x" + str(self.maze.size) + "-" +
-                             str(float("%.5f" % self.duration)) + "s.png", "PNG")
+        super().save_png()
 
     def print_results(self):
-        print("RECURSIVE BACKTRACKER: %s x %s" % (self.maze.size, self.maze.size))
-        print("--- %s seconds ---" % self.duration)
-        print("History saved to", self.directory)
+        super().print_results()
+
+    def maze_generator_factory(self, size: int):
+        pass
+
+
+
+generator = GeneratePrimms(11)
+generator.generate()
+
+#to run right click file and run file
